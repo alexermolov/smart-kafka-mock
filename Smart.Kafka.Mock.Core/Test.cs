5a99dfa94b581c1922be046c0b024ae1d897ccc5
Aleksei Ermolov <aleksei.ermolov.smr@gmail.com>
2023-08-16 20:03:38 +0400

    init commit

---
+++ Smart.Kafka.Mock.Core/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+using JUST;
+
+namespace Smart.Kafka.Mock.Core
+{
+    public class Test
+    {
+        public void Transform()
+        {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            //read input from JSON file
+            string input = File.ReadAllText($"{path}/schema.json");
+
+            //read the transformer from a JSON file
+            string transformer = File.ReadAllText($"{path}/transformer.json");
+
+            // do the actual transformation [equal to new JsonTransformer<JsonPathSelectable>(...) for backward compatibility]
+            string transformedString = JsonTransformer.Transform(transformer, input);
+
+            var test = 0;
+        }
+    }
+}
---
+++ Smart.Kafka.Mock/BackgroundServices/TestBackgroundWorker.cs
@@ -0,0 +1,13 @@
+﻿using Smart.Kafka.Mock.Core;
+
+namespace Smart.Kafka.Mock.BackgroundServices
+{
+    public class TestBackgroundWorker : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            new Test().Transform();
+            return Task.CompletedTask;
+        }
+    }
+}
